7d712ce769a6e2a3e829d4859876288936172e4a
Vincent Yu <vinceyu1228@gmail.com>
2021-04-20 12:14:33 +0800

    checked function finished

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,8 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  const ul = document.querySelector('ul')
+
+  ul.addEventListener('click', (e)=> {
+    e.target.classList.toggle('checked')
+  })
+})
