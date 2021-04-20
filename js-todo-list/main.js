bac2f54e3dcefe2efd66541e4d2bdfeda4a4eb19
Vincent Yu <vinceyu1228@gmail.com>
2021-04-20 13:45:20 +0800

    finished add function

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,32 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+  const ul = document.querySelector('ul')
+  const input = document.querySelector('#input')
+  const btn = document.querySelector('#addBtn')
+
+  ul.addEventListener('click', (e)=> {
+    const targetTagName = e.target.tagName
+    switch (targetTagName) {
+      case 'LI':
+        e.target.classList.toggle('checked')  
+        break;
+      case 'SPAN':
+        ul.removeChild(e.target.parentNode)
+        break;
+    }
+  })
+  
+  btn.addEventListener('click', function() {
+    const inputContent = document.createTextNode(input.value)
+    const li = document.createElement('li')
+    const close = document.createElement('span')
+    
+    li.appendChild(inputContent)
+
+    close.setAttribute('class', 'close')
+    close.appendChild(document.createTextNode('x'))
+    
+    li.appendChild(close)
+    ul.appendChild(li)
+  })
+})
