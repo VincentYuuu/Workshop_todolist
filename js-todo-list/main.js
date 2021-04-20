bf7da7266d6ba9d380eeae7659f52abb487ec2c9
Vincent Yu <vinceyu1228@gmail.com>
2021-04-20 16:44:23 +0800

    for loop finished

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -33,40 +33,40 @@
 //  })
 //})
 
-//document.addEventListener('DOMContentLoaded', function() {
-//  const ul = document.querySelector('ul')
-//  const li = document.querySelectorAll('li')
-//  const input = document.querySelector('#input')
-//  const btn = document.querySelector('#addBtn')
-//  const close = document.querySelectorAll('span.close')
+document.addEventListener('DOMContentLoaded', function() {
+  const ul = document.querySelector('ul')
+  const li = document.querySelectorAll('li')
+  const input = document.querySelector('#input')
+  const btn = document.querySelector('#addBtn')
+  const close = document.querySelectorAll('span.close')
 
-//  for (let i = 0; i < li.length; i++) {
-//    li[i].addEventListener('click', function() {
-//      this.classList.toggle('checked') 
-//    })
-//    close[i].addEventListener('click', function() {
-//      this.parentNode.remove()
-//    })
-//  }
+  for (let i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', function() {
+      this.classList.toggle('checked') 
+    })
+    close[i].addEventListener('click', function() {
+      this.parentNode.remove()
+    })
+  }
 
     
-//    btn.addEventListener('click', function() {
-//      const newLi = document.createElement('li')
-//      const inputValue = document.createTextNode(input.value)
-//      const newClose = document.createElement('span')
-//      newLi.addEventListener('click', function() {
-//        this.classList.toggle('checked') 
-//      })
-//      newClose.addEventListener('click', function() {
-//        this.parentNode.remove()
-//      })
+    btn.addEventListener('click', function() {
+      const newLi = document.createElement('li')
+      const inputValue = document.createTextNode(input.value)
+      const newClose = document.createElement('span')
+      newLi.addEventListener('click', function() {
+        this.classList.toggle('checked') 
+      })
+      newClose.addEventListener('click', function() {
+        this.parentNode.remove()
+      })
       
-//      newLi.appendChild(inputValue)
+      newLi.appendChild(inputValue)
       
-//      newClose.setAttribute('class', 'close')
-//      newClose.appendChild(document.createTextNode('x'))
+      newClose.setAttribute('class', 'close')
+      newClose.appendChild(document.createTextNode('x'))
   
-//      newLi.appendChild(newClose)
-//      ul.appendChild(newLi)
-//    })
-//})
+      newLi.appendChild(newClose)
+      ul.appendChild(newLi)
+    })
+})
